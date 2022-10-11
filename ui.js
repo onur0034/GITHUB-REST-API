4b64c7cc5f6748003b08e800278a43de8bb7c1bd
onur <onur.un0034@gmail.com>
2022-10-11 22:10:48 +0300

    inspect

--- ui.js
+++ ui.js
@@ -1,22 +1,23 @@
 class UI {
   constructor() {
+    this.cardBody = document.querySelector(".card-body");
     this.profileDiv = document.getElementById("profile");
     this.repoDiv = document.getElementById("repos");
-    this.LastUsers = document.getElementById("last-users");
+    this.lastUsers = document.getElementById("last-users");
     this.inputField = document.getElementById("githubname");
-    this.cardBody = document.querySelector(".card-body");
-
-    //this at constructors*****
   }
 
   clearInput() {
-    this.inputField.value = "";
+    this.inputField = "";
   }
+
   showUserInfo(user) {
-    this.profileDiv.innerHTML = `   <div class="card card-body mb-3">
+    this.profileDiv.innerHTML = `
+        
+        <div class="card card-body mb-3">
                     <div class="row">
                       <div class="col-md-4">
-                        <a href="${user.html_url}" target = "_blank">
+                        <a href="" target = "_blank">
                          <img class="img-fluid mb-2" src="${user.avatar_url}"> </a>
                          <hr>
                          <div id="fullName"><strong> ${user.name}</strong></div>
@@ -40,11 +41,11 @@ class UI {
                                     
                                 </li>
                                 <li class="list-group-item borderzero">
-                                    <img src="images/location.png" width="30px"> <span id = "location">${user.location}</a>
+                                    <img src="images/location.png" width="30px"> ${user.location}<span id = "location">sadasdsad</a>
                                     
                                 </li>
                                 <li class="list-group-item borderzero">
-                                    <img src="images/mail.png" width="30px"> <span id="mail">${user.email}</span>
+                                    <img src="images/mail.png" width="30px"> <span id="company">${user.email}</span>
                                     
                                 </li>
                                 
@@ -52,15 +53,16 @@ class UI {
                                
                             
                       </div>
-                </div>         `;
+                </div>
+        `;
   }
 
   showAlert(message) {
-    const div = document.createElement("div");
-    div.className = "alert alert-danger";
+    let div = document.createElement("div");
+    div.className("alert alert-warning");
+
     div.textContent = message;
     this.cardBody.appendChild(div);
-
     setTimeout(() => {
       div.remove();
     }, 2000);
